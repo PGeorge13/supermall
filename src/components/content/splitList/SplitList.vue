961b128f7acbed0ff4898bb3d60f1a38a61e599f
bqy <2475988405@qq.com>
2021-10-05 17:57:12 +0800

    封装组件 swiper（轮播图）、SplitList（展示多列数据）、GoodsList（商品列表）等，封装首页网络请求，保存请求的数据，并将数据传入首页的组件中进行展示。

---
+++ src/components/content/splitList/SplitList.vue
@@ -0,0 +1,58 @@
+<template>
+<!-- 这个组件没有选择使用插槽，因为组件很简单，只有文字不同，传入一个数组即可使用 -->
+  <div class="split-list">
+    <div v-for="(item, index) in titles" :key="index"
+    class="split-list-item"
+    :class="{active: index === currentIndex}"
+    @click="itemClick(index)">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "SplitList",
+    data() {
+      return {
+        currentIndex: 0,
+      }
+    },
+    props: {
+      titles: {
+        type: Array,
+        default() {
+          return [];
+        },
+      }
+    },
+    methods: {
+      itemClick(index) {
+        this.currentIndex = index;
+        this.$emit('splitClick', index)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .split-list {
+    display: flex;
+    text-align: center;
+    height: 40px;
+    line-height: 40px;
+    font-size: 15px;
+    background-color: rgb(255, 255, 255);
+  }
+  .split-list-item {
+    flex: 1;
+  }
+  .split-list-item span{
+    padding: 5px;
+  }
+  .active span{
+    color: var(--color-high-text);
+    border-bottom: 3px solid var(--color-tint);
+  }
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,29 +1,209 @@
 <template>
-  <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+  <div id="home">  
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners = "banners"></home-swiper>
+    <home-recommend :recommends = "recommends"></home-recommend>
+    <home-feature></home-feature>
+    <split-list :titles="['流行', '新款', '精选']" 
+    class="split-list"
+    @splitClick="splitClick"></split-list>
+    <goods-list :goods="showGoods"></goods-list>
+
+    <ul>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+      <li>列表1</li>
+    </ul>
+  </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar';
-  import {getHomeMultidata} from "network/home";
+  import SplitList from 'components/content/splitList/SplitList';
+  import GoodsList from 'components/content/goods/GoodsList';
+
+  import HomeSwiper from './childComps/HomeSwiper.vue';
+  import HomeRecommend from './childComps/HomeRecommend.vue';
+  import HomeFeature from './childComps/HomeFeature.vue';
+
+  import {getHomeMultidata, getHomeGoods} from "network/home";
 
   export default {
     name: 'Home',
+    components: {
+      NavBar,
+      SplitList,
+      HomeSwiper,
+      HomeRecommend,
+      HomeFeature,
+      GoodsList,
+    },
     data() {
       return {
         banners: [],
         recommends: [],
+        goods: {
+          'pop': {page: 0, list: []},
+          'new': {page: 0, list: []},
+          'sell': {page: 0, list: []},
+        },
+        currentType: 'pop',
       }
     },
-    components: {
-      NavBar,
-    },
+    computed: {
+      showGoods() {
+        return this.goods[this.currentType].list;
+      }
+    },  
     created() {
-      getHomeMultidata().then(res => {
-        console.log(res);
-        this.banners = res.data.banner.list;
-        this.recommends = res.data.recommend.list
-      });
-    }
+      // 请求首页多个数据
+      this.getHomeMultidata();
+      // 请求首页商品数据
+      this.getHomeGoods('pop');
+      this.getHomeGoods('new');
+      this.getHomeGoods('sell');
+    },
+    methods: {
+      /**
+       * 事件监听相关方法
+       */
+      splitClick(index) {
+        switch(index) {
+          case 0:
+            this.currentType = 'pop';
+            break;
+          case 1:
+            this.currentType = 'new';
+            break;
+          case 2: 
+            this.currentType = 'sell';
+            break;
+        }
+      },
+      /**
+       * 网络请求相关方法
+       */
+      getHomeMultidata() {
+        getHomeMultidata().then(res => {
+          // console.log(res);
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list
+        });
+      },
+      getHomeGoods(type) {
+        const page = this.goods[type].page + 1
+        getHomeGoods(type, page).then(res => {
+          // console.log(res);
+          this.goods[type].list.push(...res.data.list)
+          this.goods[type].page += 1;
+        })
+      }
+    },
   }
 </script>
 
@@ -31,5 +211,19 @@
   .home-nav {
      background-color: var(--color-tint);
      color: #fff;
+
+     position: fixed;
+     top: 0;
+     left: 0;
+     right: 0;
+     z-index: 9;
+  }
+  #home {
+    padding-top: 44px;
+  }
+  .split-list {
+    position: sticky;
+    top: 44px;
+    z-index: 9;
   }
 </style>
